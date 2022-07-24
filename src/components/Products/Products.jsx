1fa9be983dc71b874dc1af110cd8a78ef713d3d8
Tomer <37521314+tomerssh@users.noreply.github.com>
2022-07-24 20:30:18 +0300

    add navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
 	{ id: 1, name: 'Shoes', description: 'Running Shoes.', price: '$5', image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d7057e9f-0723-4cc4-8387-8e70c93c3379/air-zoom-alphafly-next-2-proto-road-racing-shoes-8c1M69.png' },
@@ -9,8 +10,10 @@ const products = [
 ];
 
 const Products = () => {
+	const classes = useStyles();
 	return (
-		<main>
+		<main className={classes.content}>
+			<div className={classes.toolbar} />
 			<Grid container justifyContent='center' spacing={4}>
 				{products.map((product) => (
 					<Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
